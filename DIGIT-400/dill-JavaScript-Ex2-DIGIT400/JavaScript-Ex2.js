bbcc0679e9ab4445aac5929d843532724c9a8cab
Dill <ged5131@psu.edu>
2021-10-22 15:25:39 -0400

    made some edits NOT DONE

--- DIGIT-400/dill-JavaScript-Ex2-DIGIT400/JavaScript-Ex2.js
+++ DIGIT-400/dill-JavaScript-Ex2-DIGIT400/JavaScript-Ex2.js
@@ -1,12 +1,20 @@
-document.addEventListener( 'DOMContentLoaded',pageLoadUp,false);
+window.addEventListener( 'DOMContentLoaded',pageLoadUp,false);
  var search=document.getElementById("content")
 
 function pageLoadUp() {
     alert ('The page loaded');
+    var input = document.getElementById("input")
+    {input[0].addEventListner("click", searchMe, false)}
 } 
 let p = document.getElementById("p");
-function search (){
-let input = document.getElementById("p");
+
+function searchMe(){
+let input = document.getElementById("input");
+let filter = input.value.toUpperCase();
+let searchArea=document.getElementById("p1")
+
+if (searchArea[0].innerHTML.toUpperCase().indexOf(filter) > -1) searchArea [0]
+
 if (input !==''){
     let regExp = new RegExp(input, "gi");
     p.innerHTML = (p.textContent).replace(regExp, "<mark>${searched}</mark>'");
