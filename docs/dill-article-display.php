6228533596b2acd14814c92a9645c84cefb47a38
Dill <ged5131@psu.edu>
2021-11-01 12:07:28 -0400

    added 2nd php file

---
+++ docs/dill-article-display.php
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>Article Display</h1>
+        <ul>
+            <?php require_once("config.php");
+    $contents = REST_PATH . "/db//ged5131/dill-article-displayPHP.xql";
+    $result = file_get_contents($contents);
+    echo $result;
+?></ul>
+<?php include("dill-phpEx2-DIGIT-400-REDO.php");?>
+    </body>
+</html>
